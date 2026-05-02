da65bdc2e910c501fdb4cf4e0903219ace0004de
Dhruv Sharma <2006.dhruvsharma@gmail.com>
2026-05-02 10:50:57 +0000

    Complete the basic load balancer & update README.md

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Load Balancer
 
-A Basic Load Balancer written in Go.
+A Basic Load Balancer written in Go. The main aim of this project is to learn how load balancer(at least, the basic ones) function.
+The secondary aim was to deepen my understanding of Go.
+
+## How to use
+1) Create a basic server (or multiple) like this (Keep them in separate directories):
+```go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Response from Server 1")
+	})
+	http.ListenAndServe(":8081", nil)
+}
+
+```
+
+2) Run the server(s)
+3) Run the Load Balancer 
+```bash
+go run main.go -port=3030 -backends=http://localhost:8081
+```
+4) Check the functioning in the terminal where the load balancer is running & using `curl http://localhost:3030` in a separate terminal.
+
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -43,6 +47,21 @@ func (b *Backend) IsAlive() (alive bool) {
 
 	alive = b.Alive
 	return
+}
+
+// Add a backend to the server pool
+func (s *ServerPool) AddBackend(backend *Backend) {
+	s.backends = append(s.backends, backend)
+}
+
+// Changes a status of the given backend
+func (s *ServerPool) MarkBackendStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.backends {
+		if b.URL.String() == backendUrl.String() {
+			b.SetAlive(alive)
+			break
+		}
+	}
 }
 
 // The value stays b/w 0 and the length of the slice
@@ -131,9 +150,9 @@ func lb(w http.ResponseWriter, r *http.Request) {
 func healthCheck() {
 	t := time.NewTicker(time.Second * 20)
 
-	for {
-		select {
-		case <-t.C:
+	for { //Infinite loop
+		select { //Used for when more cases will be added later
+		case <-t.C: //channel, runs after each tick
 			log.Println("Starting health check")
 			serverPool.HealthCheck()
 			log.Println("Health check completed")
@@ -142,3 +161,70 @@ func healthCheck() {
 }
 
 var serverPool ServerPool
+
+func main() {
+	var serverList string
+	var port int
+
+	//Command line arguments, Ex -> go run main.go -port=8080 -backends=http://localhost:8081,http://localhost:8082
+	flag.StringVar(&serverList, "backends", "", "Load balanced backends, use commas to separate")
+	flag.IntVar(&port, "port", 3030, "Port to serve")
+
+	flag.Parse()
+
+	if len(serverList) == 0 {
+		log.Fatal("Please provide one or more backends to load balance")
+	}
+
+	//parse the servers
+	tokens := strings.Split(serverList, ",")
+	for _, tok := range tokens {
+		serverUrl, err := url.Parse(tok)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(serverUrl)
+		proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, e error) {
+			log.Printf("[%s] %s\n", serverUrl.Host, e.Error())
+			retries := GetRetryFromContext(request)
+
+			if retries > 3 {
+				select {
+				case <-time.After(10 * time.Millisecond):
+					ctx := context.WithValue(request.Context(), Retry, retries+1)
+					proxy.ServeHTTP(writer, request.WithContext(ctx))
+				}
+			}
+
+			serverPool.MarkBackendStatus(serverUrl, false)
+			attempts := GetAttemptsFromContext(request)
+
+			log.Printf("%s(%s) Attempting retry %d\n", request.RemoteAddr, request.URL.Path, attempts)
+			ctx := context.WithValue(request.Context(), Attempts, attempts+1)
+			lb(writer, request.WithContext(ctx))
+		}
+
+		serverPool.AddBackend(&Backend{
+			URL:          serverUrl,
+			Alive:        true,
+			ReverseProxy: proxy,
+		})
+		log.Printf("Configured server: %s\n", serverUrl)
+	}
+
+	//Start http server
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(lb),
+	}
+
+	//Start health check
+	go healthCheck()
+
+	log.Printf("Load Balancer started at :%d\n", port)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
